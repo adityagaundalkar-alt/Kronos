d5e34a4dee81dd373224483923e82dcc7a92e964
Aditya Gaundalkar <adityagaundalkar@Adityas-MacBook-Air.local>
2026-04-07 14:22:12 +0700

    Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD0ZbCo4swkchf51jr0Jrh6DfrExwGw93Q",
   authDomain: "kronos-fcada.firebaseapp.com",
@@ -8,3 +16,7 @@ const firebaseConfig = {
   appId: "1:1087373672487:web:ae8af09338753081d54e02",
   measurementId: "G-PYLCGVQJZL"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
